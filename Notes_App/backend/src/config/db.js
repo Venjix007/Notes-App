25ce0e16c80790b9a6a9c048e33df5add188837f
Venjix007 <abhaypatel5845@gmail.com>
2025-09-09 14:52:49 +0530

    Initial commit

---
+++ Notes_App/backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MONGODB CONNECTED SUCCESSFULLY!");
+  } catch (error) {
+    console.error("Error connecting to MONGODB", error);
+    process.exit(1); // exit with failure
+  }
+};
